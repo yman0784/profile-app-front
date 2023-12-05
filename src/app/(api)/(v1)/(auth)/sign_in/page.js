a4a0ddd9766f1c0554251b90bded92aa4963210d
yman0784 <tykesam@yahoo.co.jp>
2023-12-05 15:28:36 +0900

    event修正 コメントアウト

--- src/app/(api)/(v1)/(auth)/sign_in/page.js
+++ src/app/(api)/(v1)/(auth)/sign_in/page.js
@@ -13,10 +13,10 @@ import Footer from "@/components/atoms/layouts/Footer/Footer";
 const Login = () => {
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
-  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setInputEmail(event.target.value);
-  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setInputPassword(event.target.value);
+  // const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) =>
+  //   setInputEmail(event.target.value);
+  // const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) =>
+  //   setInputPassword(event.target.value);
   const router = useRouter();
   const [loginError, setLoginError] = useState("");
   const [showToast, setShowToast] = useState(false);

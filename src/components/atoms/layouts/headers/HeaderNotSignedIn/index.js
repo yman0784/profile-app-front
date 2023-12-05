e5ce1a85c5f922acd9542a0444db93a39a75b409
yman0784 <tykesam@yahoo.co.jp>
2023-12-05 14:15:16 +0900

    ページごとのヘッダー変更

--- src/components/atoms/layouts/headers/HeaderNotSignedIn/index.js
+++ src/components/atoms/layouts/headers/HeaderNotSignedIn/index.js
@@ -5,17 +5,19 @@ import styles from "./index.module.css";
 const HeaderNotSignedIn = () => {
   return (
     <header>
-      <div className={styles.gamingbackgroundcolor}>
+      <div className={styles.header}>
         <div className={styles.logo}>
           <Link href="/" className={styles.a}>
             My Portfolio
             {/* <img src="logo.png" alt="Logo" /> */}
           </Link>
         </div>
-        <div className="login-button">
-          <Link href="/sign_in">
-            <button>ログイン</button>
-          </Link>
+        <div className={styles.headerRight}>
+          <div className={styles.buttonWrapper}>
+            <Link href="/sign_in">
+              <button className={styles.button}>ログイン</button>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
